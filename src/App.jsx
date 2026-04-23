9c9868a53260b1c5b29d242a9f674322af490402
Steevens Alce <alcesteevens2022@gmail.com>
2026-04-23 15:32:58 -0400

    Initialisation de la page de reservation

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+﻿import { useState } from 'react'
+import Reservation from './pages/Reservation'
+import ReservationConfirmation from './pages/ReservationConfirmation'
+import ReservationSuccess from './pages/ReservationSuccess'
+
+function App() {
+  const [reservedDriver, setReservedDriver] = useState(null)
+  const [page, setPage] = useState('reservation')
+
+  return (
+    <div>
+      {page === 'reservation' && (
+        <Reservation
+          onReserve={(driver) => {
+            setReservedDriver(driver)
+            setPage('confirmation')
+          }}
+        />
+      )}
+      {page === 'confirmation' && reservedDriver && (
+        <ReservationConfirmation
+          driver={reservedDriver}
+          onBack={() => setPage('reservation')}
+          onConfirm={() => setPage('success')}
+        />
+      )}
+      {page === 'success' && reservedDriver && (
+        <ReservationSuccess
+          driver={reservedDriver}
+          onDone={() => {
+            setReservedDriver(null)
+            setPage('reservation')
+          }}
+        />
+      )}
+    </div>
+  )
+}
+
+export default App;
