4162dab42a42452d5a2132c2cb0dcc63fd514178
Djenica <djenicaexilus@gmail.com>
2026-04-24 13:58:06 -0400

    update Accueil page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Accueil from "./pages/Accueil";
-import Connexion from "./pages/Connexion";
-import Inscription from "./pages/Inscription";
-import Réservation from "./pages/Réservation";
-import Paiement from "./pages/Paiement";
-import "./App.css";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Accueil from "./pages/Accueil";
+// import Connexion from "./pages/Connexion";
+// import Inscription from "./pages/Inscription";
+// import Réservation from "./pages/Réservation";
+// import Paiement from "./pages/Paiement";
+// import Profil from "./pages/Profil";
+// import "./App.css";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Accueil />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="/inscription" element={<Inscription />} />
-        <Route path="/reservation" element={<Réservation />} />
-        <Route path="/paiement" element={<Paiement />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Accueil />} />
+//         <Route path="/connexion" element={<Connexion />} />
+//         <Route path="/inscription" element={<Inscription />} />
+//         <Route path="/reservation" element={<Réservation />} />
+//         <Route path="/paiement" element={<Paiement />} />
+//         <Route path="/Profil" element={<Profil />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
